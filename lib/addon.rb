f0e244f6537cb1012b9e4092d7b673830d5c4528
unknown <dave@textgoeshere.org.uk>
2009-01-06 18:11:48 +0100

    Fixed .jar name

--- lib/addon.rb
+++ lib/addon.rb
@@ -1,4 +1,4 @@
-ADDON_NAME = "streetmap.xpi"
+ADDON_NAME = "streetmap"
 
 SRC    = "src"
 BUILD  = "build"
@@ -7,12 +7,12 @@ namespace :addon do
   desc "Packages addon as .xpi from source via .jar"
   task :build do |t|
     cd File.join(SRC, 'chrome')
-    system "zip -rD #{File.join("..", "..", BUILD, 'chrome', 'chrome.jar')} ."
+    system "zip -rD #{File.join("..", "..", BUILD, 'chrome', "#{ADDON_NAME}.jar")} ."
     cd File.join("..", "..")
     cp File.join(SRC, 'chrome.manifest'), File.join(BUILD)
     cp File.join(SRC, 'install.rdf'), File.join(BUILD)
     cd BUILD
-    system "zip -r #{File.join("..", ADDON_NAME)} ."
+    system "zip -r #{File.join("..", "#{ADDON_NAME}.xpi")} ."
   end
 end
 
